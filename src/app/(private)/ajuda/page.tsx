597862552a10862df04cfd6556e9755a8cac7bbd
hotcaffe <raphaelfusco.rar@gmail.com>
2024-02-07 17:56:53 -0300

    Adicionado: Tela de SAC

---
+++ src/app/(private)/ajuda/page.tsx
@@ -0,0 +1,64 @@
+"use client"
+
+import { FormBody } from "@/components/common/FormBody";
+import { FormInput } from "@/components/common/FormInput";
+import { MaskedInput } from "@/components/common/MaskedInput";
+import { Button, Center, HStack, Input, Text, Textarea, VStack } from "@chakra-ui/react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import * as Yup from 'yup'
+
+interface ITicket {
+    vc_titulo: string;
+    vc_contato: number;
+    vc_descri: string;
+}
+
+const schema = Yup.object().shape({
+    vc_titulo: Yup.string().min(6, 'O título deve ter no mínimo 6 caracteres').required('O título é obrigatório!'),
+    vc_contato: Yup.number().typeError('Númerico').min(11000000000, "Digite o telefone completo!").required('O telefone é obrigatório!'),
+    vc_descri: Yup.string().min(6, 'A descrição deve ter no mínimo 20 caracteres').required('A descrição é obrigatória!')
+})
+
+export default function Page() {
+    const {register, handleSubmit, formState} = useForm<ITicket>({
+        mode: 'all',
+        resolver: yupResolver(schema)
+    });
+
+    const {errors} = formState;
+
+    async function onSubmit(data: ITicket) {
+        console.log(data)
+    }
+
+    return (
+        <Center w="100%">
+            <VStack bg="white" w="1000px" minH="600px" p="20px" align="start">
+                <FormBody title="Entre em contato com o suporte:" titleDivider w="100%">
+                    <Center w="100%" my="25px">
+                        <VStack as="form" align="start" gap="25px">
+                            <Text fontSize="18px" fontWeight="semibold" color="teal.800">Abra um ticket:</Text>
+                            <HStack>
+                                <FormInput title='Título' error={errors?.vc_titulo?.message} w="300px" h="80px" man>
+                                    <Input placeholder="Digite o título do seu problema" type="text" {...register("vc_titulo")}/>
+                                </FormInput>
+                                <FormInput title='Contato' error={errors?.vc_contato?.message} w="200px" h="80px" man>
+                                    <MaskedInput 
+                                        placeholder="Digite seu telefone"
+                                        mask="(00) 00000-0000"
+                                        {...register("vc_contato")}
+                                    />
+                                </FormInput>
+                            </HStack>
+                            <FormInput title='Descreva o seu problema' error={errors?.vc_descri?.message} w="100%" man>
+                                <Textarea h="100px" placeholder="Descreva e detalhe o seu problema" {...register("vc_descri")}/>
+                            </FormInput>
+                            <Button w="100%" type="submit" onClick={handleSubmit(onSubmit, () => console.log(errors))}>Enviar</Button>
+                        </VStack>
+                    </Center>
+                </FormBody>
+            </VStack>
+        </Center>
+    )
+}
