be43b1bf9c749c51f71d3db88b8c7c47af19f527
hotcaffe <raphaelfusco.rar@gmail.com>
2024-04-06 12:51:28 -0300

    Adicionado: Integração da tela de login com o backend

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,35 +1,79 @@
 'use client'
 
+import { FormInput } from "@/components/common/FormInput";
 import { TAPin } from "@/config/icons";
-import { Flex, Center, createIcon, Heading, Input, Button, Text, FormControl, FormLabel, Checkbox, HStack, Link, Spacer } from "@chakra-ui/react";
+import { api } from "@/utils/api";
+import { Flex, Center, createIcon, Heading, Input, Button, Text, FormControl, FormLabel, Checkbox, HStack, Link, Spacer, InputGroup, InputRightAddon, Icon, InputRightElement } from "@chakra-ui/react";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Eye, EyeOff } from "react-feather";
+import { useForm } from "react-hook-form";
+import * as Yup from 'yup'
 
+interface ILogin {
+    username: string;
+    password: string
+}
+
+const schema = Yup.object().shape({
+    username: Yup.string().required("É obrigatório informar o usuário!"),
+    password: Yup.string().required("É obrigatório inserir a senha!")
+})
 
 export default function Page() {
     const router = useRouter()
+    const {register, handleSubmit, formState, setValue, setError, clearErrors} = useForm<ILogin>({
+        mode: 'all',
+        resolver: yupResolver(schema)
+    });
+    const [hidePassword, setHidePassword] = useState(true);
+
+    const {errors} = formState
+
+    function toggleHidePassword(e: any) {
+        e.preventDefault();
+        e.stopPropagation();
+        setHidePassword(!hidePassword);
+    }
+
+    async function login({username, password}: ILogin) {
+        console.log(username, password)
+        await api.get('/user/login', {
+            params: {
+                username,
+                password
+            }
+        })
+
+        router.push("/")
+    }
 
     return (
         <Center h="100vh" flexDirection="column" gap="20px">
             <Link  fontWeight="bold" href="/">Retornar para a página principal</Link>
             <Flex w="800px" h="600px">
-                <Flex direction="column" align="center" justify="center" w="500px" gap="15px" rounded="10px 0 0 10px" bg="white">
+                <Flex as="form" direction="column" align="center" justify="center" w="500px" gap="15px" rounded="10px 0 0 10px" bg="white">
                     <TAPin w="48px" h="48px" mb="15px" />
                     <Heading mb="30px" fontSize="xl" color="teal.300" fontWeight="semibold">Informe seus dados para entrar</Heading>
-                    <FormControl w="400px">
-                        <FormLabel>Email</FormLabel>
-                        <Input placeholder="Ex: seuemail@dominio.com" type="email"/>
-                    </FormControl>
-                    <FormControl w="400px">
-                        <FormLabel>Senha</FormLabel>
-                        <Input placeholder="Digite a sua senha" type="password"/>
-                    </FormControl>
+                    <FormInput title="Usuário" w="400px" error={errors?.username?.message} man>
+                        <Input placeholder="Informe seu usuário" type="text" {...register("username")}/>
+                    </FormInput>
+                    <FormInput title="Senha" w="400px" error={errors?.password?.message} man>
+                        <InputGroup>
+                            <Input placeholder="Digite sua senha" type={hidePassword ? "password" : "text"} {...register("password")}/>
+                            <InputRightElement cursor="pointer" onClick={(e) => toggleHidePassword(e)}>
+                                <Icon as={hidePassword ? Eye : EyeOff} color="gray.600"/>
+                            </InputRightElement>
+                        </InputGroup>
+                    </FormInput>
                     <HStack w="400px">
                         <Checkbox>Manter conectado?</Checkbox>
                         <Spacer/>
                         <Link>Esqueci minha senha</Link>
                     </HStack>
                     
-                    <Button mt="50px" w="100px" >Entrar</Button>
+                    <Button mt="50px" w="100px" onClick={handleSubmit(login)} >Entrar</Button>
                 </Flex>
                 <Flex direction="column" align="center" justify="center" w="290px" gap="20px" px="35px" bg="teal.300" rounded="0 10px 10px 0">
                     <Heading fontSize="3xl" mb="20px" color="white" w="100%">Bem vindo ao<br/>Troque-Agora!</Heading>
